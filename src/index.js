b011064577f9b3cce77f7568658f473b1e0bab2b
Lesley Chard <lac275@mun.ca>
2018-08-27 11:04:48 -0230

    Added theming and advanced Button styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {ThemeProvider} from 'react-jss';
 import App from './App';
+import theme from './theme';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = (
+    <ThemeProvider theme={theme}>
+        <App />
+    </ThemeProvider>
+);
+
+ReactDOM.render(Root, document.getElementById('root'));
